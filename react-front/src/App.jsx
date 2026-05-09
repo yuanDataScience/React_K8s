4ad002d04f14da0eb8f49b29cb619267e6407486
yuanMLOps <yuanaws.2023@gmail.com>
2026-05-08 22:23:46 -0400

    initial push

---
+++ react-front/src/App.jsx
@@ -0,0 +1,43 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom"
+
+import AuthRequired from "./components/AuthRequired"
+
+import RootLayout from "./layouts/RootLayout"
+
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+
+import NotFound from "./pages/NotFound"
+import PlotlyCharts from "./pages/PlotlyCharts"
+import D3Charts from "./pages/D3Charts"
+import { AuthProvider } from "./contexts/AuthContext"
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      
+      <Route path="login" element={<Login />} />
+     
+      <Route path="plotly-demo" element={<PlotlyCharts />} />
+      <Route path="d3-demo" element={ <D3Charts />} />
+     
+      <Route path="*" element={<NotFound />} />         
+    </Route>
+  )
+)
+
+export default function App() {
+  return (
+    <AuthProvider>
+       <RouterProvider router={router} />
+    </AuthProvider>
+    
+  )
+}
---
+++ react-front/src/layouts/RootLayout.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Outlet,
+         NavLink
+ } from 'react-router-dom'
+ import { useAuth } from "../hooks/useAuth"
+
+const RootLayout = () => {
+  const { user, message, logout } = useAuth()
+
+  return (
+    <div className="bg-blue-200 min-h-screen p-2">
+        <h2>RootLayout</h2>
+        <p className="text-red-500 p-2 border">
+            {message}
+        </p>
+        <p>Username: {user}</p>
+        <header className="p-8 w-full">
+            <nav className="flex flex-row justify-between">
+                <div className="flex flex-row space-x-3">
+                    <NavLink to="/">Home</NavLink>
+                    
+                    { user? <>
+                     
+                      <button onClick={logout}>Logout</button>
+                    </>:<>
+                    <NavLink to="/login">Login</NavLink>
+                    </>
+                    }
+                    <NavLink to="/plotly-demo"> plotly Demo</NavLink> 
+                    <NavLink to="/D3-demo"> D3 Demo</NavLink> 
+                </div>
+            </nav>
+        </header>
+        <main className="p-8 flex flex-col flex-1 bg-white">
+            <Outlet />
+        </main>
+    
+    </div>
+  )
+}
+
+export default RootLayout
